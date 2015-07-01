a57504a318545b595a64adae863a17379cbf98ed
Devin Wadsworth <devinwadsworth@gmail.com>
2015-07-01 10:13:32 -0700

    index and delete endpoints

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,15 @@ var express = require('express'),
 // db
 var Item = require('mongoose').model('Item');
 
+// items#index
+router.get('/items', function (request, response) {
+  Item.find().exec(function(error, items) {
+    if (error) return response.status(404).send(error);
+    response.status(200).json(items)
+  });
+});
+
+// items#create
 router.post('/items', function (request, response) {
   var item = new Item({
     title: request.body.title
@@ -13,6 +22,14 @@ router.post('/items', function (request, response) {
   item.save(function (error, item) {
     if (error) throw error;
     response.status(201).json(item);
+  });
+});
+
+// items#destroy
+router.delete('/items/:id', function (request, response) {
+  Item.findByIdAndRemove(request.params.id, function (error, item) {
+    if (error) return response.status(404).send(error);
+    response.status(200).send();
   });
 });
 
