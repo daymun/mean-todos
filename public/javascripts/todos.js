2513803f41f70c6199a14fce10f37068c2f6a433
Devin Wadsworth <devinwadsworth@gmail.com>
2015-07-02 09:45:53 -0700

    update indentation

--- public/javascripts/todos.js
+++ public/javascripts/todos.js
@@ -1,35 +1,35 @@
 var meanTodos = angular.module('meanTodos', []);
 
 angular.module('meanTodos', [])
-       .controller('mainController', function ($scope, $http) {
-         $scope.formData = {};
+  .controller('mainController', function ($scope, $http) {
+    $scope.formData = {};
 
-         // make todo items available to the view
-         $http.get('/api/items')
-              .success(function (data) {
-                $scope.items = data;
-                $scope.countItems();
-                console.log(data);
-              })
-              .error(function (data) {
-                console.log('Error: ' + data);
-              });
+    // make todo items available to the view
+    $http.get('/api/items')
+      .success(function (data) {
+        $scope.items = data;
+        $scope.countItems();
+        console.log(data);
+      })
+      .error(function (data) {
+        console.log('Error: ' + data);
+      });
 
-         $scope.countItems = function () {
-           $('.count-todos').html($scope.items.length);
-         }
+    $scope.countItems = function () {
+      $('.count-todos').html($scope.items.length);
+    }
 
-         $scope.createItem = function () {
-           console.log($scope.formData)
-           $http.post('/api/items', $scope.formData)
-                .success(function (data) {
-                  $scope.formData = {};
-                  $scope.items.push(data);
-                  $scope.countItems();
-                  console.log(data);
-                })
-                .error(function (data) {
-                  console.log('Error: ' + data);
-                });
-         };
-       });
+    $scope.createItem = function () {
+      console.log($scope.formData);
+      $http.post('/api/items', $scope.formData)
+        .success(function (data) {
+          $scope.formData = {};
+          $scope.items.push(data);
+          $scope.countItems();
+          console.log(data);
+        })
+        .error(function (data) {
+          console.log('Error: ' + data);
+        });
+    };
+  });
